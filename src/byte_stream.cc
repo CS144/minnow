94851d31eea20950a453ab3f0d16f7a1f13d8e82
Ran Zhou <rzhoujames@163.com>
2023-05-28 13:33:31 +0000

    9/10 test passed

--- src/byte_stream.cc
+++ src/byte_stream.cc
@@ -1,77 +1,97 @@
-#include <stdexcept>
-
 #include "byte_stream.hh"
-
+#include <cstdint>
+#include <stdexcept>
+#include <string_view>
 using namespace std;
-
-ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ) {}
-
+ByteStream::ByteStream( uint64_t capacity ) 
+:capacity_(capacity),buf_(0),close_(false),error_(false),bytes_popped_(0),bytes_pushed_(0){
+}
 void Writer::push( string data )
 {
-  // Your code here.
-  (void)data;
+  if(error_){
+    throw runtime_error("The stream has error. Push failed.\n");
+  }
+  if(close_){
+   // throw runtime_error("The stream is closed. Push failed.\n");
+   return;
+  }
+  if(available_capacity()>=data.size()){
+    for(const auto& d:data){
+      buf_.push_back(d);
+    }
+    bytes_pushed_+=data.size();
+  }
+  else{//cap<data.size
+    for(auto it=data.begin();it!=data.begin()+static_cast<int64_t>(available_capacity());++it){
+      buf_.push_back(*it);
+    }
+    bytes_pushed_+=available_capacity();
+  }
 }
-
 void Writer::close()
 {
-  // Your code here.
+  close_=true; 
 }
-
 void Writer::set_error()
 {
-  // Your code here.
+  error_=true;
 }
-
 bool Writer::is_closed() const
 {
-  // Your code here.
-  return {};
+  return close_;
 }
-
 uint64_t Writer::available_capacity() const
 {
-  // Your code here.
-  return {};
+  return capacity_-(bytes_pushed_-bytes_popped_);
 }
-
 uint64_t Writer::bytes_pushed() const
 {
-  // Your code here.
-  return {};
+  return bytes_pushed_;
 }
-
 string_view Reader::peek() const
 {
-  // Your code here.
-  return {};
+  static string ss;
+  ss.clear();
+  for(auto it=buf_.begin();it!=buf_.begin()+static_cast<int64_t>(bytes_buffered());++it){
+    ss+=*it;
+  }
+  return ss;
 }
-
 bool Reader::is_finished() const
 {
-  // Your code here.
-  return {};
+  return close_&&!bytes_buffered();
 }
-
 bool Reader::has_error() const
 {
-  // Your code here.
-  return {};
+  return error_;
 }
-
 void Reader::pop( uint64_t len )
 {
-  // Your code here.
-  (void)len;
+  if(has_error()){
+    throw runtime_error("The stream has error. Read failed.\n");
+  }
+  if(is_finished()){
+    //throw runtime_error("The stream is finished. Read failed.\n");
+    return;
+  }
+  if(bytes_buffered()>=len){
+    for(uint64_t i=0;i<len;++i){
+      buf_.pop_front();
+    }
+    bytes_popped_+=len;
+  }
+  else{//buf<len
+    while(!buf_.empty()) { 
+      buf_.pop_front();
+    }
+    bytes_popped_+=bytes_buffered();
+  }
 }
-
-uint64_t Reader::bytes_buffered() const
+ uint64_t Reader::bytes_buffered() const
 {
-  // Your code here.
-  return {};
+  return bytes_pushed_-bytes_popped_;
 }
-
 uint64_t Reader::bytes_popped() const
 {
-  // Your code here.
-  return {};
+  return bytes_popped_;
 }
--- src/byte_stream_helpers.cc
+++ src/byte_stream_helpers.cc
@@ -1,8 +1,6 @@
 #include "byte_stream.hh"
-
 #include <cstdint>
 #include <stdexcept>
-
 /*
  * read: A helper function thats peeks and pops up to `len` bytes
  * from a ByteStream Reader into a string;
@@ -10,20 +8,16 @@
 void read( Reader& reader, uint64_t len, std::string& out )
 {
   out.clear();
-
   while ( reader.bytes_buffered() and out.size() < len ) {
     auto view = reader.peek();
-
     if ( view.empty() ) {
       throw std::runtime_error( "Reader::peek() returned empty string_view" );
     }
-
     view = view.substr( 0, len - out.size() ); // Don't return more bytes than desired.
     out += view;
     reader.pop( view.size() );
   }
 }
-
 Reader& ByteStream::reader()
 {
   static_assert( sizeof( Reader ) == sizeof( ByteStream ),
@@ -31,7 +25,6 @@ Reader& ByteStream::reader()
 
   return static_cast<Reader&>( *this ); // NOLINT(*-downcast)
 }
-
 const Reader& ByteStream::reader() const
 {
   static_assert( sizeof( Reader ) == sizeof( ByteStream ),
@@ -39,7 +32,6 @@ const Reader& ByteStream::reader() const
 
   return static_cast<const Reader&>( *this ); // NOLINT(*-downcast)
 }
-
 Writer& ByteStream::writer()
 {
   static_assert( sizeof( Writer ) == sizeof( ByteStream ),
@@ -47,7 +39,6 @@ Writer& ByteStream::writer()
 
   return static_cast<Writer&>( *this ); // NOLINT(*-downcast)
 }
-
 const Writer& ByteStream::writer() const
 {
   static_assert( sizeof( Writer ) == sizeof( ByteStream ),
