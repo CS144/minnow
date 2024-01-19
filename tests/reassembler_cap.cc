b1f6da3764817e5546b52041e888b5d253f8597e
Taeuk Kang <code@taeuk.net>
2024-01-19 00:01:48 -0800

    Add test for inserting beyond capacity with unacceptable data mixed

--- tests/reassembler_cap.cc
+++ tests/reassembler_cap.cc
@@ -120,6 +120,31 @@ int main()
 
       test.execute( IsFinished { true } );
     }
+
+    {
+      ReassemblerTestHarness test { "insert beyond capacity with unacceptable data mixed", 2 };
+
+      test.execute( Insert { "e", 1 } );
+      test.execute( Insert { "d", 5 }.is_last() ); // unacceptable, so "d" is not remembered
+      test.execute( BytesPending( 1 ) );
+      test.execute( BytesPushed( 0 ) );
+      test.execute( IsFinished { false } );
+
+      test.execute( Insert { "lelqnk", 0 }.is_last() ); // "lqnk" unacceptable
+      test.execute( BytesPushed( 2 ) );
+      test.execute( BytesPending( 0 ) );
+      test.execute( ReadAll( "le" ) );
+      test.execute( IsFinished { false } );
+
+      test.execute( Insert { "lat", 2 } ); // "t" unacceptable
+      test.execute( BytesPushed( 4 ) );
+      test.execute( ReadAll( "la" ) );
+
+      test.execute( Insert { "nd", 4 }.is_last() );
+      test.execute( BytesPushed( 6 ) );
+      test.execute( ReadAll( "nd" ) );
+      test.execute( IsFinished { true } );
+    }
   } catch ( const exception& e ) {
     cerr << "Exception: " << e.what() << endl;
     return EXIT_FAILURE;
