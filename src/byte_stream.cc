95cdfd4e06b9fd95499f62206497c8111d235620
lawine <854340647@qq.com>
2024-03-12 11:50:19 +0000

    pass check0 and modi

--- src/byte_stream.cc
+++ src/byte_stream.cc
@@ -6,63 +6,63 @@ ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ) {}
 
 bool Writer::is_closed() const
 {
-  return this->write_have_been_closed;
+  return write_have_been_closed;
 }
 
 void Writer::push( string data )
 {
   uint64_t data_length =data.size();
-  if(!this->is_closed() && data_length<=this->capacity_){
-    this->stream_data.append(data);
-    this->capacity_-=data_length;
-    this->total_bytes_pushed+=data_length;
-  }else if (!this->is_closed() && data_length>this->capacity_){
-    data=data.substr(0,this->capacity_);
-    this->stream_data.append(data);
-    this->total_bytes_pushed+=this->capacity_;
-    this->capacity_=0;
+  if(!is_closed() && data_length<=capacity_){
+    stream_data.append(data);
+    capacity_-=data_length;
+    total_bytes_pushed+=data_length;
+  }else if (!is_closed() && data_length>capacity_){
+    data=data.substr(0,capacity_);
+    stream_data.append(data);
+    total_bytes_pushed+=capacity_;
+    capacity_=0;
   }
   return;
 }
 
 void Writer::close()
 {
-    this->write_have_been_closed=true;
+    write_have_been_closed=true;
 }
 
 uint64_t Writer::available_capacity() const
 {
-  return this->capacity_;
+  return capacity_;
 }
 
 uint64_t Writer::bytes_pushed() const
 {
-  return this->total_bytes_pushed;
+  return total_bytes_pushed;
 }
 
 bool Reader::is_finished() const
 {
-  return this->write_have_been_closed && !this->stream_data.size();
+  return write_have_been_closed && !stream_data.size();
 }
 
 uint64_t Reader::bytes_popped() const
 {
-  return this->total_bytes_poped;
+  return total_bytes_poped;
 }
 
 string_view Reader::peek() const
 {
-  return this->stream_data;
+  return stream_data;
 }
 
 void Reader::pop( uint64_t len )
 {
-  this->stream_data=this->stream_data.substr(len);
-  this->total_bytes_poped+=len;
-  this->capacity_+=len;
+  stream_data=stream_data.substr(len);
+  total_bytes_poped+=len;
+  capacity_+=len;
 }
 
 uint64_t Reader::bytes_buffered() const
 {
-  return this->total_bytes_pushed-this->total_bytes_poped;
+  return total_bytes_pushed-total_bytes_poped;
 }
